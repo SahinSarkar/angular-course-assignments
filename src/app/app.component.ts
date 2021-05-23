b846f83e1ea1efc4e2187d07a29e86ac23cae58e
SahinSarkar <sarkarsahin697971@gmail.com>
2021-05-23 22:57:28 +0530

    Assignment 3 - Implementation of assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showContent = false;
+  clicks = 0;
+  logEntries = [];
+  toggleShowContent() {
+    this.showContent = !this.showContent;
+    this.clicks++;
+    this.logEntries.push('Button clicked ' + this.clicks + ' times. ' + new Date());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

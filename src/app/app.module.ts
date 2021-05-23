e6b60f74c9dbd8173fd4974868dc8deb7ea57dde
SahinSarkar <sarkarsahin697971@gmail.com>
2021-05-23 18:33:04 +0530

    First commit of assignment 2 to make it equal to initial state of assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-
-import {AppComponent} from './app.component';
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
-import { ErrorAlertComponent } from './error-alert/error-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    ErrorAlertComponent,
-    SuccessAlertComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

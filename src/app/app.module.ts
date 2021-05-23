822fb93644faee664699b73fa3b258861b9dbd49
SahinSarkar <sarkarsahin697971@gmail.com>
2021-05-23 14:06:51 +0530

    Added code to implement Assignment 1, to create and style 2 components and call them in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    ErrorAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
